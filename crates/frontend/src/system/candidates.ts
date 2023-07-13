718a1cf65ac0d3e61b2358f6fd75efb842497186
Robin van Boven <497556+Beanow@users.noreply.github.com>
2023-07-13 10:15:50 +0200

    Closes the 2023 vote (#4)

--- crates/frontend/src/system/candidates.ts
+++ crates/frontend/src/system/candidates.ts
@@ -10,34 +10,34 @@ export interface Candidate {
 }
 
 export const CANDIDATES: Candidate[] = [
-  {
-    id: "david-lemarier",
-    name: "David Lemarier",
-    avatar: "avatar-david.jpg",
-    intro_url: `${CANDIDATE_PAGE}#david-lemarier`,
-  },
-  {
-    id: "bill-avery",
-    name: "Bill Avery",
-    avatar: "avatar-bill.png",
-    intro_url: `${CANDIDATE_PAGE}#bill-avery`,
-  },
-  {
-    id: "jacob-bolda",
-    name: "Jacob Bolda",
-    avatar: "avatar-jacob.jpg",
-    intro_url: `${CANDIDATE_PAGE}#jacob-bolda`,
-  },
-  {
-    id: "zack-chapple",
-    name: "Zack Chapple",
-    avatar: "avatar-zack.jpg",
-    intro_url: `${CANDIDATE_PAGE}#zack-chapple`,
-  },
-  {
-    id: "robin-van-boven",
-    name: "Robin van Boven",
-    avatar: "avatar-robin.png",
-    intro_url: `${CANDIDATE_PAGE}#robin-van-boven`,
-  },
+  // {
+  //   id: "david-lemarier",
+  //   name: "David Lemarier",
+  //   avatar: "avatar-david.jpg",
+  //   intro_url: `${CANDIDATE_PAGE}#david-lemarier`,
+  // },
+  // {
+  //   id: "bill-avery",
+  //   name: "Bill Avery",
+  //   avatar: "avatar-bill.png",
+  //   intro_url: `${CANDIDATE_PAGE}#bill-avery`,
+  // },
+  // {
+  //   id: "jacob-bolda",
+  //   name: "Jacob Bolda",
+  //   avatar: "avatar-jacob.jpg",
+  //   intro_url: `${CANDIDATE_PAGE}#jacob-bolda`,
+  // },
+  // {
+  //   id: "zack-chapple",
+  //   name: "Zack Chapple",
+  //   avatar: "avatar-zack.jpg",
+  //   intro_url: `${CANDIDATE_PAGE}#zack-chapple`,
+  // },
+  // {
+  //   id: "robin-van-boven",
+  //   name: "Robin van Boven",
+  //   avatar: "avatar-robin.png",
+  //   intro_url: `${CANDIDATE_PAGE}#robin-van-boven`,
+  // },
 ];
