b2cf39a1fe14f2dbf811b3195dfb3c5644813f98
Jacob Bolda <me@jacobbolda.com>
2024-07-07 22:32:47 -0500

    board vote 2024

--- crates/frontend/src/system/candidates.ts
+++ crates/frontend/src/system/candidates.ts
@@ -1,5 +1,5 @@
 export const REPO_URL = "https://github.com/tauri-apps/governance-and-guidance";
-export const CANDIDATE_PAGE = `${REPO_URL}/blob/main/board-election-2023/Candidates.md`;
+export const CANDIDATE_PAGE = `${REPO_URL}/blob/main/board-election-2024/Candidates.md`;
 
 export type CandidateId = string;
 export interface Candidate {
@@ -10,34 +10,16 @@ export interface Candidate {
 }
 
 export const CANDIDATES: Candidate[] = [
-  // {
-  //   id: "david-lemarier",
-  //   name: "David Lemarier",
-  //   avatar: "avatar-david.jpg",
-  //   intro_url: `${CANDIDATE_PAGE}#david-lemarier`,
-  // },
-  // {
-  //   id: "bill-avery",
-  //   name: "Bill Avery",
-  //   avatar: "avatar-bill.png",
-  //   intro_url: `${CANDIDATE_PAGE}#bill-avery`,
-  // },
-  // {
-  //   id: "jacob-bolda",
-  //   name: "Jacob Bolda",
-  //   avatar: "avatar-jacob.jpg",
-  //   intro_url: `${CANDIDATE_PAGE}#jacob-bolda`,
-  // },
-  // {
-  //   id: "zack-chapple",
-  //   name: "Zack Chapple",
-  //   avatar: "avatar-zack.jpg",
-  //   intro_url: `${CANDIDATE_PAGE}#zack-chapple`,
-  // },
-  // {
-  //   id: "robin-van-boven",
-  //   name: "Robin van Boven",
-  //   avatar: "avatar-robin.png",
-  //   intro_url: `${CANDIDATE_PAGE}#robin-van-boven`,
-  // },
+  {
+    id: "daniel-thompson-yvetot",
+    name: "Daniel Thompson-Yvetot",
+    avatar: "35242872",
+    intro_url: `${CANDIDATE_PAGE}#daniel-thompson-yvetot`,
+  },
+  {
+    id: "lucas-nogueira",
+    name: "Lucas Nogueira",
+    avatar: "20051258",
+    intro_url: `${CANDIDATE_PAGE}#lucas-nogueira`,
+  },
 ];
